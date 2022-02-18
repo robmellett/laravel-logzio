2cd817dd35af38c7f95522be00de7e13c5af839b
Rob Mellett <dev@robmellett.com>
2022-02-18 19:18:37 +0700

    Add json message formatter

---
+++ src/Formatters/JsonMessageFormatter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RobMellett\Logzio\Formatters;
+
+use GuzzleHttp\MessageFormatter;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+
+class JsonMessageFormatter extends MessageFormatter
+{
+    public function format(
+        RequestInterface $request,
+        ?ResponseInterface $response = null,
+        ?Throwable $error = null
+    ): string {
+        return json_encode([
+            "id" => "",
+            "method" => $request->getMethod(),
+            "uri" => $request->getUri()->__toString(),
+            "headers" => $request->getHeaders(),
+            "body" => $request->getBody()->__toString()
+        ]);
+    }
+}
